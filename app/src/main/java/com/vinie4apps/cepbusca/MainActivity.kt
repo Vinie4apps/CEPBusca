16aaebdc136c5808059f25556de6d5d7ac5e442a
Vinie4apps <vinie4apps@gmail.com>
2021-05-01 12:41:54 -0300

    Commit

--- app/src/main/java/com/vinie4apps/cepbusca/MainActivity.kt
+++ app/src/main/java/com/vinie4apps/cepbusca/MainActivity.kt
@@ -4,44 +4,75 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONException
 
 
-private var requestQueue: RequestQueue? = null
+var requestQueue: RequestQueue? = null
 @SuppressLint("StaticFieldLeak")
 lateinit var textView: TextView
+private lateinit var textUF: String
 
 
-class MainActivity : AppCompatActivity() {
+class BuscacepActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         requestQueue = Volley.newRequestQueue(this)
 
+        val cidade = findViewById<View>(R.id.cidade) as EditText
+        val strcidade = cidade.text
+
+        val end = findViewById<View>(R.id.end) as EditText
+        val strend = end.text
+
         val editText = findViewById<View>(R.id.campo) as EditText
         val str = editText.text
 
-        val bnt = findViewById<Button>(R.id.button_parse)
+        val card = findViewById<CardView>(R.id.cardv)
+
+        val bntcep = findViewById<Button>(R.id.bntcep)
+        val bntend = findViewById<Button>(R.id.bntend)
+
         textView = findViewById<View>(R.id.text_view_result) as TextView
 
 
-        bnt.setOnClickListener {
-            jsonParse(str)
+        bntcep.setOnClickListener {
+            card.visibility = View.VISIBLE
+            jsonParseCEP(str)
 
         }
+        bntend.setOnClickListener {
+            card.visibility = View.VISIBLE
+            jsonParseEND(strcidade, strend)
 
+        }
+        val spinner = findViewById<Spinner>(R.id.uf)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.UF, R.layout.support_simple_spinner_dropdown_item
+        )
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
     }
 
-    private fun jsonParse(str: Editable) {
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        textUF = parent.getItemAtPosition(position).toString()
+    }
+
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+    private fun jsonParseCEP(str: Editable) {
         textView.text = " "
         val url = "https://viacep.com.br/ws/$str/json/"
         val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
@@ -57,6 +88,27 @@ class MainActivity : AppCompatActivity() {
                 val siafi = response.getString("siafi")
 
                 textView.append("Logradouro: $log\n Bairro: $bairro\n Complemento: $complemento\n Local: $local\n UF: $uf\n IBGE: $ibge\n DDD: $ddd\n SIAFI: $siafi")
+
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+                textView.append("CEP não encontrado :(")
+            }
+        }, { error -> error.printStackTrace() })
+        requestQueue?.add(request)
+    }
+    private fun jsonParseEND(cidade: Editable, end: Editable) {
+        textView.text = " "
+        val url = "https://viacep.com.br/ws/$textUF/$cidade/$end/json/"
+        val request = JsonArrayRequest(Request.Method.GET, url, null, { response ->
+            try {
+
+                val cep = response.getJSONObject(0).getString("cep")
+                val bairro = response.getJSONObject(0).getString("bairro")
+                val ibge = response.getJSONObject(0).getString("ibge")
+                val ddd = response.getJSONObject(0).getString("ddd")
+                val siafi = response.getJSONObject(0).getString("siafi")
+                textView.append("CEP: $cep\n Bairro: $bairro\n IBGE: $ibge\n DDD: $ddd\n SIAFI: $siafi")
 
 
             } catch (e: JSONException) {
